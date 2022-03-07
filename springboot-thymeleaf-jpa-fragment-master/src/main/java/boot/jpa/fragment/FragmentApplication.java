9d6f0a63b3a575e04e9987d3747ba1759d9b73fd
Byun sang il <sangil8585@naver.com>
2022-03-07 16:32:19 +0900

    initialize Hero

---
+++ springboot-thymeleaf-jpa-fragment-master/src/main/java/boot/jpa/fragment/FragmentApplication.java
@@ -0,0 +1,15 @@
+package boot.jpa.fragment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class FragmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FragmentApplication.class, args);
+    }
+
+}
